9ede811e3aaa30e367cabe418816febe20041fd0
Valentin Lahaye <valentin.lahaye@gmail.com>
2022-06-25 10:21:51 +0200

    feat(codecs): add bytes value codec

---
+++ protobsoncodec/bytes_value_codec.go
@@ -0,0 +1,77 @@
+package protobsoncodec
+
+import (
+	"fmt"
+	"reflect"
+
+	"go.mongodb.org/mongo-driver/bson/bsoncodec"
+	"go.mongodb.org/mongo-driver/bson/bsonrw"
+	"go.mongodb.org/mongo-driver/bson/bsontype"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+// BytesValue type.
+var TypeBytesValue = reflect.TypeOf((*wrapperspb.BytesValue)(nil))
+
+// BytesValueCodec is the Codec used for *wrapperspb.BytesValue values.
+type BytesValueCodec struct{}
+
+// EncodeValue is the ValueEncoderFunc for *wrapperspb.BytesValue.
+func (vc *BytesValueCodec) EncodeValue(ec bsoncodec.EncodeContext, vw bsonrw.ValueWriter, v reflect.Value) error {
+	if !v.IsValid() || v.Type() != TypeBytesValue {
+		return bsoncodec.ValueEncoderError{
+			Name:     "BytesValueCodec.EncodeValue",
+			Types:    []reflect.Type{TypeBytesValue},
+			Received: v,
+		}
+	}
+	val := v.Interface().(*wrapperspb.BytesValue)
+	if val == nil {
+		return vw.WriteNull()
+	}
+	return vw.WriteBinary(val.Value)
+}
+
+// DecodeValue is the ValueDecoderFunc for *wrapperspb.BytesValue.
+func (vc *BytesValueCodec) DecodeValue(dc bsoncodec.DecodeContext, vr bsonrw.ValueReader, v reflect.Value) error {
+	if !v.CanSet() || v.Type() != TypeBytesValue {
+		return bsoncodec.ValueDecoderError{
+			Name:     "BytesValueCodec.DecodeValue",
+			Types:    []reflect.Type{TypeBytesValue},
+			Received: v,
+		}
+	}
+	val := &wrapperspb.BytesValue{}
+	switch bsonTyp := vr.Type(); bsonTyp {
+	case bsontype.Binary:
+		v, _, err := vr.ReadBinary()
+		if err != nil {
+			return err
+		}
+		val.Value = v
+	case bsontype.String:
+		v, err := vr.ReadString()
+		if err != nil {
+			return err
+		}
+		val.Value = []byte(v)
+	case bsontype.Null:
+		if err := vr.ReadNull(); err != nil {
+			return err
+		}
+		val = nil
+	case bsontype.Undefined:
+		if err := vr.ReadUndefined(); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("cannot decode %v into a *wrapperspb.BytesValue", bsonTyp)
+	}
+	v.Set(reflect.ValueOf(val))
+	return nil
+}
+
+// NewBytesValueCodec returns a BytesValueCodec.
+func NewBytesValueCodec() *BytesValueCodec {
+	return &BytesValueCodec{}
+}
